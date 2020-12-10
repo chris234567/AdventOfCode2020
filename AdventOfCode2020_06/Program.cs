7eb02a7bd2df94b68a1e5fb78e75551991799693
Christoph Stade <christoph-stade@outlook.de>
2020-12-10 22:38:59 +0100

    added solution for day 10 part 1

--- AdventOfCode2020_06/Program.cs
+++ AdventOfCode2020_06/Program.cs
@@ -16,36 +16,36 @@ namespace AdventOfCode2020_06
             {
                 Console.WriteLine(element);
             }
-            //int count = 0;
-            //int lines = 0;
-            //Dictionary<char, int> characters = new Dictionary<char, int>(); ;
+            int count = 0;
+            int lines = 0;
+            Dictionary<char, int> characters = new Dictionary<char, int>(); ;
 
-            //for (int i = 0; i < groups.Length; i++)
-            //{
-            //    if (!(groups[i] == ""))
-            //        lines++;
+            for (int i = 0; i < groups.Length; i++)
+            {
+                if (!(groups[i] == ""))
+                    lines++;
 
-            //    var curr = groups[i].ToCharArray();
+                var curr = groups[i].ToCharArray();
 
-            //    for (int l = 0; l < curr.Length; l++)
-            //        if(!characters.TryAdd(curr[l], 1))
-            //            characters[curr[l]]++;
+                for (int l = 0; l < curr.Length; l++)
+                    if (!characters.TryAdd(curr[l], 1))
+                        characters[curr[l]]++;
 
-            //    if (groups[i] == "" || i == groups.Length - 1) // next group
-            //    {
-            //        int counter = 0;
+                if (groups[i] == "" || i == groups.Length - 1) // next group
+                {
+                    int counter = 0;
 
-            //        for (char k = (char)97; k < 123; k++)
-            //            if (characters.ContainsKey(k))
-            //                //if (characters[k] == lines) // <-- Part 2
-            //                    counter++;
-                    
-            //        count += counter;
-            //        characters.Clear(); // reset
-            //        lines = 0;
-            //    }
-            //}
-            //Console.WriteLine("Count: " + count);
+                    for (char k = (char)97; k < 123; k++)
+                        if (characters.ContainsKey(k))
+                            //if (characters[k] == lines) // <-- Part 2
+                            counter++;
+
+                    count += counter;
+                    characters.Clear(); // reset
+                    lines = 0;
+                }
+            }
+            Console.WriteLine("Count: " + count);
         }
     }
 }
---
+++ AdventOfCode2020_10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AdventOfCode2020_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Chris\source\AdventOfCode2020\AdventOfCode2020_10\input_day10.txt";
+            var input = File.ReadAllLines(path).Select(s => int.Parse(s)).ToList();
+
+            input.Add(0);
+            input.Sort();
+            input.Add(input[input.Count - 1] + 3);
+
+            // PART 1
+
+            int ones = 0;
+            int threes = 0;
+
+            for (int i = 0; i < input.Count - 1; i++)
+            {
+                int diff = input[i + 1] - input[i];
+
+                if (diff == 1)
+                    ones++;
+                else if (diff == 3)
+                    threes++;
+            }
+
+            Console.WriteLine($"ones : {ones} * threes : {threes}\n = {ones * threes}");
+
+            // PART2 
+
+            // in the works...
+        }
+    }
+}
