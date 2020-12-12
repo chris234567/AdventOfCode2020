f2843672e8aca6f6665c46a6470ad88989c90b68
Christoph Stade <christoph-stade@outlook.de>
2020-12-12 13:39:13 +0100

    added some solution approaches

---
+++ AdventOfCode2020_11/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2020_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Chris\source\AdventOfCode2020\AdventOfCode2020_11\input_day11.txt";
+            var input = File.ReadAllLines(path);
+
+            char[,] myCharArrayOrigin = new char[input.Length, input[0].Length];
+
+            for (int y = 0; y < myCharArrayOrigin.GetLength(0); y++) // for easier indexing and cause of string immutability
+            {
+                for (int x = 0; x < myCharArrayOrigin.GetLength(1); x++)
+                {
+                    myCharArrayOrigin[x, y] = input[x][y];
+                }
+            }
+
+            char[,] myCharArray = new char[input.Length, input[0].Length];
+
+            for (int y = 0; y < myCharArray.GetLength(0); y++) // copy oder so was
+            {
+                for (int x = 0; x < myCharArray.GetLength(1); x++)
+                {
+                    myCharArray[x, y] = myCharArrayOrigin[x, y];
+                }
+            }
+
+
+            for (int y = 0; y < input.Length; y++)
+            {
+                for (int x = 0; x < input[0].Length; x++)
+                {
+                    if (y == 0 || y != myCharArray.GetLength(0) - 1 && x == 0 || x == myCharArray.GetLength(1) - 1)
+                        if (myCharArray[x, y] == 'L')
+                            myCharArray[x, y] = '#';
+                        else
+                        {
+                            if (myCharArray[x, y] == 'L') // empty seat
+                                if (y != 0 && myCharArray[x, y - 1] != '#') // no adjacent seat on x axis
+                                    if (y != myCharArray.GetLength(0) - 1 && myCharArray[x, y + 1] != '#')
+                                        if (x != 0 && myCharArray[x - 1, y] != '#') // no adjacent seat on y axis
+                                            if (x != myCharArray.GetLength(1) - 1 && myCharArray[x + 1, y] != '#')
+
+                                                myCharArray[x, y] = '#';
+                        }
+                }
+            }
+
+            for (int i = 0; i < myCharArray.GetLength(0); i++)
+            {
+                for (int e = 0; e < myCharArray.GetLength(1); e++)
+                {
+                    Console.Write(myCharArray[i, e]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+// Anderer Loesungsansatz
+
+//using System;
+//using System.IO;
+
+//namespace AdventOfCode2020_11
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string path = @"C:\Users\Chris\source\AdventOfCode2020\AdventOfCode2020_11\input_day11.txt";
+//            var input = File.ReadAllLines(path);
+
+//            char[,] myCharArray = new char[input.Length, input[0].Length];
+
+//            for (int y = 0; y < myCharArray.GetLength(0); y++) // for easier indexing and cause of string immutability
+//            {
+//                for (int x = 0; x < myCharArray.GetLength(1); x++)
+//                {
+//                    myCharArray[x, y] = input[x][y];
+//                }
+//            }
+
+//            // Rule 1
+
+//            for (int y = 0; y < myCharArray.GetLength(0); y++)
+//            {
+//                for (int x = 0; x < myCharArray.GetLength(1); x++)
+//                {
+//                    int counter = 0;
+
+//                    if (myCharArray[x, y] == 'L') // empty seat
+
+//                        if (y == 0) // first col
+//                            // irgentwas
+//                            continue;
+
+//                    if (myCharArray[y, x - 1] != '#')
+
+//                        if (y != myCharArray.GetLength(0) - 1) // last col
+
+//                            if (myCharArray[x, y + 1] != '#')
+
+//                                if (x != 0) // first row
+
+//                                    if (myCharArray[x - 1, y] != '#')
+
+//                                        if (x != myCharArray.GetLength(1) - 1) // last col
+
+//                                            if (myCharArray[x + 1, y] != '#')
+
+//                                                myCharArray[x, y] = '#';
+//                }
+//            }
+
+//            // Rule 2
+
+//            //for (int y = 0; y < input.Length; y++)
+//            //{
+//            //    for (int x = 0; x < input[0].Length; x++)
+//            //    {
+//            //        int counter = 0;
+
+//            //        if (myCharArray[x, y] == '#') // occupied seat
+
+//            //            if (y != 0) // first col
+//            //            {
+//            //                if (myCharArray[x, y - 1] == '#')
+//            //                    if (y != myCharArray.GetLength(0) - 1) // last col
+//            //                    {
+//            //                        if (myCharArray[x, y + 1] == '#')
+//            //                            if (x != 0) // first row
+//            //                            {
+//            //                                if (myCharArray[x - 1, y] == '#')
+//            //                                    if (x != myCharArray.GetLength(1) - 1) // last col
+//            //                                    {
+//            //                                        if (myCharArray[x + 1, y] == '#')
+
+//            //                                            counter++;
+//            //                                    }
+//            //                                    else
+//            //                                        counter++;
+//            //                            }
+//            //                            else
+//            //                                counter++;
+//            //                    }
+//            //                    else
+//            //                        counter++;
+//            //            }
+//            //            else
+//            //                counter++;
+
+//            //        if (counter > 3)
+//            //            myCharArray[x, y] = 'L';
+//            //    }
+//            //}
+
+//            // Printout
+
+//            for (int i = 0; i < myCharArray.GetLength(0); i++)
+//            {
+//                for (int e = 0; e < myCharArray.GetLength(1); e++)
+//                {
+//                    Console.Write(myCharArray[i, e]);
+//                }
+//                Console.WriteLine();
+//            }
+//        }
+//    }
+//}
---
+++ AdventOfCode2020_12/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2020_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Chris\source\AdventOfCode2020\AdventOfCode2020_12\input_day12.txt";
+            var navigationRoute = File.ReadAllLines(path);
+
+            int north = 0;
+            int east = 0;
+            int south = 0;
+            int west = 0;
+            string[] directions = new string[] { "north", "east", "south", "west" }; // for better imagination
+            int currDirection = 1;
+            int currDegree;
+
+            foreach (var instruction in navigationRoute)
+            {
+                char currChar = instruction[0];
+
+                if (currChar == 'R' || currChar == 'L') // turning
+                {
+                    currDegree = Int32.Parse(instruction.Trim('R', 'L')) % 360;
+
+                    switch (currDegree)
+                    {
+                        case 0:
+                            break;
+                        case 90:
+                            if (currChar == 'R')
+                                currDirection = (currDirection + 1) % 4;
+                            else if (currChar == 'L')
+                                currDirection = Math.Abs((currDirection - 1) % 4);
+                            break;
+                        case 180:
+                            if (currChar == 'R')
+                                currDirection = (currDirection + 2) % 4;
+                            else if (currChar == 'L')
+                                currDirection = Math.Abs((currDirection - 2) % 4);
+                            break;
+                        case 270:
+                            if (currChar == 'R')
+                                currDirection = (currDirection + 3) % 4;
+                            else if (currChar == 'L')
+                                currDirection = Math.Abs((currDirection - 3) % 4);
+                            break;
+                    }
+                }
+                else // moving
+                {
+                    if (currChar == 'F')
+                    {
+                        switch (currDirection)
+                        {
+                            case 0:
+                                north += Int32.Parse(instruction.Trim('F'));
+                                break;
+                            case 1:
+                                east += Int32.Parse(instruction.Trim('F'));
+                                break;
+                            case 2:
+                                south += Int32.Parse(instruction.Trim('F'));
+                                break;
+                            case 3:
+                                west += Int32.Parse(instruction.Trim('F'));
+                                break;
+                        }
+                    }
+                    else if (currChar == 'N')
+                    {
+                        north += Int32.Parse(instruction.Trim('N'));
+                    }
+                    else if (currChar == 'E')
+                    {
+                        east += Int32.Parse(instruction.Trim('E'));
+                    }
+                    else if (currChar == 'S')
+                    {
+                        south += Int32.Parse(instruction.Trim('S'));
+                    }
+                    else if (currChar == 'W')
+                    {
+                        west += Int32.Parse(instruction.Trim('W'));
+                    }
+                }
+            }
+
+            int posX = Math.Abs(west - east);
+            int posY = Math.Abs(north - south);
+
+            Console.WriteLine(posX + posY);
+        }
+    } // 1996, 3128, (3820, 3948, 4632 ( too high )) false
+}
