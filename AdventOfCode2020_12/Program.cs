3d30d76e02857553311610094da85832be6d4160
Christoph Stade <christoph-stade@outlook.de>
2020-12-13 10:45:41 +0100

    added solution for day 13 part 1 and some approaches for part 2

--- AdventOfCode2020_12/Program.cs
+++ AdventOfCode2020_12/Program.cs
@@ -8,14 +8,20 @@ namespace AdventOfCode2020_12
         static void Main(string[] args)
         {
             string path = @"C:\Users\Chris\source\AdventOfCode2020\AdventOfCode2020_12\input_day12.txt";
-            var navigationRoute = File.ReadAllLines(path);
+            string[] navigationRoute = File.ReadAllLines(path);
 
+            SolvePart1(navigationRoute);
+            SolvePart2(navigationRoute);
+        }
+
+        static void SolvePart1(string[] navigationRoute)
+        {
             int north = 0;
             int east = 0;
             int south = 0;
             int west = 0;
-            string[] directions = new string[] { "north", "east", "south", "west" }; // for better imagination
-            int currDirection = 1;
+            string[] directions = new string[] { "north", "east", "south", "west" }; // for better imagination // could be implemented as an enum...
+            int currDirection = 1; // facing east at start
             int currDegree;
 
             foreach (var instruction in navigationRoute)
@@ -28,25 +34,23 @@ namespace AdventOfCode2020_12
 
                     switch (currDegree)
                     {
-                        case 0:
-                            break;
                         case 90:
                             if (currChar == 'R')
                                 currDirection = (currDirection + 1) % 4;
                             else if (currChar == 'L')
-                                currDirection = Math.Abs((currDirection - 1) % 4);
+                                currDirection = (currDirection - 1 + 4) % 4; // why doesnt math.abs work?!
                             break;
                         case 180:
                             if (currChar == 'R')
                                 currDirection = (currDirection + 2) % 4;
                             else if (currChar == 'L')
-                                currDirection = Math.Abs((currDirection - 2) % 4);
+                                currDirection = (currDirection - 2 + 4) % 4;
                             break;
                         case 270:
                             if (currChar == 'R')
                                 currDirection = (currDirection + 3) % 4;
                             else if (currChar == 'L')
-                                currDirection = Math.Abs((currDirection - 3) % 4);
+                                currDirection = (currDirection - 3 + 4) % 4;
                             break;
                     }
                 }
@@ -94,5 +98,173 @@ namespace AdventOfCode2020_12
 
             Console.WriteLine(posX + posY);
         }
-    } // 1996, 3128, (3820, 3948, 4632 ( too high )) false
+        static void SolvePart2(string[] navigationRoute)
+        {
+            int north = 0;
+            int east = 0;
+            int south = 0;
+            int west = 0;
+            string[] directions = new string[] { "north", "east", "south", "west" }; // for better imagination
+            int currDirection1 = 1; // waypoint facing part east
+            int currDirection2 = 0; // and part north at start
+            int currDegree;
+
+            int[] waypoint = new int[] { 10, 1, 0, 0}; // 10 east 1 north
+
+            foreach (var instruction in navigationRoute)
+            {
+                char currChar = instruction[0];
+
+                if (currChar == 'R' || currChar == 'L') // turning
+                {
+                    currDegree = Int32.Parse(instruction.Trim('R', 'L')) % 360;
+                    int value = 0; // positions to be shifted
+
+                    foreach (var item in waypoint)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                    switch (currDegree)
+                    {
+                        case 90:
+                            if (currChar == 'R')
+                            {
+                                currDirection1 = (currDirection1 + 1) % 4;
+                                currDirection2 = (currDirection2 + 1) % 4;
+                                value = 1;
+                            }
+                            else if (currChar == 'L')
+                            {
+                                currDirection1 = (currDirection1 - 1 + 4) % 4;
+                                currDirection2 = (currDirection2 - 1 + 4) % 4;
+                                value = - 1;
+                            }
+                            break;
+                        case 180:
+                            if (currChar == 'R')
+                            {
+                                currDirection1 = (currDirection1 + 2) % 4;
+                                currDirection2 = (currDirection2 + 2) % 4;
+                                value = 2;
+                            }
+                            else if (currChar == 'L')
+                            {
+                                currDirection1 = (currDirection1 - 2 + 4) % 4;
+                                currDirection2 = (currDirection2 - 2 + 4) % 4;
+                                value = -2;
+                            }
+                            break;
+                        case 270:
+                            if (currChar == 'R')
+                            {
+                                currDirection1 = (currDirection1 + 3) % 4;
+                                currDirection2 = (currDirection2 + 3) % 4;
+                                value = 3;
+                            }
+                            else if (currChar == 'L')
+                            {
+                                currDirection1 = (currDirection1 - 3 + 4) % 4;
+                                currDirection2 = (currDirection2 - 3 + 4) % 4;
+                                value = -3;
+                            }
+                            break;
+                    }
+
+                    if (value > 0)
+                    {
+                        int[] temp = new int[waypoint.Length];
+
+                        for (int i = 0; i < waypoint.Length; i++)
+                        {
+                            temp[i] = waypoint[Math.Abs((i - value)) % waypoint.Length];
+                        }
+
+                        waypoint = temp;
+                    } 
+                    else if (value < 0)
+                    {
+                        int[] temp = new int[waypoint.Length];
+
+                        for (int i = 0; i < waypoint.Length; i++)
+                        {
+                            temp[i] = waypoint[(i - value + 4) % waypoint.Length];
+                        }
+
+                        waypoint = temp;
+                    }
+                    Console.WriteLine();
+                    foreach (var item in waypoint)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.WriteLine();
+                }
+                else // moving
+                {
+                    if (currChar == 'F')
+                    {
+                        switch (currDirection1)
+                        {
+                            case 0:
+                                north += waypoint[0] * Int32.Parse(instruction.Trim('F'));
+                                break;
+                            case 1:
+                                east += waypoint[1] * Int32.Parse(instruction.Trim('F'));
+                                break;
+                            case 2:
+                                south += waypoint[2] * Int32.Parse(instruction.Trim('F'));
+                                break;
+                            case 3:
+                                west += waypoint[3] * Int32.Parse(instruction.Trim('F'));
+                                break;
+                        }
+                        switch (currDirection2)
+                        {
+                            case 0:
+                                north += waypoint[0] * Int32.Parse(instruction.Trim('F'));
+                                break;
+                            case 1:
+                                east += waypoint[1] * Int32.Parse(instruction.Trim('F'));
+                                break;
+                            case 2:
+                                south += waypoint[2] * Int32.Parse(instruction.Trim('F'));
+                                break;
+                            case 3:
+                                west += waypoint[3] * Int32.Parse(instruction.Trim('F'));
+                                break;
+                        }
+                    }
+                    else if (currChar == 'N')
+                    {
+                        waypoint[0] += Int32.Parse(instruction.Trim('N'));
+                    }
+                    else if (currChar == 'E')
+                    {
+                        waypoint[1] += Int32.Parse(instruction.Trim('E'));
+                    }
+                    else if (currChar == 'S')
+                    {
+                        waypoint[2] += Int32.Parse(instruction.Trim('S'));
+                    }
+                    else if (currChar == 'W')
+                    {
+                        waypoint[3] += Int32.Parse(instruction.Trim('W'));
+                    }
+                }
+            }
+
+            int posX = Math.Abs(west - east);
+            int posY = Math.Abs(north - south);
+
+            Console.WriteLine(posX + posY);
+        }
+        //public static int[] RotateVector(int[] waypoint, double degree)
+        //{
+        //    waypoint[0] = waypoint[0] * Convert.ToInt32(Math.Cos(degree)) - waypoint[1] * Convert.ToInt32(Math.Sin(degree)); // x
+        //    waypoint[1] = waypoint[0] * Convert.ToInt32(Math.Sin(degree)) + waypoint[1] * Convert.ToInt32(Math.Cos(degree)); // y
+
+        //    return waypoint;
+        //}
+    }
 }
---
+++ AdventOfCode2020_13/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(@"C:\Users\Chris\source\AdventOfCode2020\AdventOfCode2020_13\input_day13.txt");
+
+            int earliestDeparture = Int32.Parse(input[0]);
+            var times = input[1].Split(',');
+            var buses = new List<int>();
+            var busIDS = new List<int>();
+
+            foreach (var bus in times)
+            {
+                if (bus != "x")
+                {
+                    buses.Add(Int32.Parse(bus));
+                    busIDS.Add(Int32.Parse(bus));
+                }
+            }
+
+            // PART 1
+
+            for (int i = 0; i < buses.Count; i++)
+            {
+                buses[i] = buses[i] * (earliestDeparture / buses[i] + 1);
+            }
+
+            int diff = earliestDeparture;
+            int currIndex = 0;
+
+            foreach (var bus in buses)
+            {
+                if (bus - earliestDeparture < diff)
+                {
+                    diff = bus - earliestDeparture;
+                    currIndex = buses.IndexOf(bus);
+                }
+            }
+
+            Console.WriteLine("Bus ID * minutes waiting : " + diff * busIDS[currIndex]);
+
+            // PART 2 // in the works
+
+            busIDS = new List<int>();
+            var remainders = new List<int>();
+            int counter = 0;
+
+            foreach (var bus in times)
+            {
+                if (bus != "x")
+                {
+                    busIDS.Add(Int32.Parse(bus));
+                    remainders.Add(counter);
+                }
+
+                //else
+                //{
+                //    busIDS.Add(0); // needed for iteration
+                //}
+
+                counter++;
+            }
+
+            Console.WriteLine(findMinX(busIDS, remainders, busIDS.Count));
+
+            // iterative solution approach 1
+
+            //while (true) 
+            //{
+            //    int counter = 0;
+
+            //    foreach (var ID in busIDS)
+            //    {
+            //        if (ID == 0)
+            //            counter++;
+
+            //        else if ((earliestTimestamp + counter) % ID == 0)
+            //            counter++;
+            //    }
+
+            //    if (counter == busIDS.Count)
+            //        break;
+
+            //    earliestTimestamp += busIDS[0];
+            //}
+
+            // iterative solution approach 2
+
+            static long findMinX(List<int> num, List<int> rem, int k)
+            {
+                long x = 100000000000000;
+
+                while (true)
+                {
+                    int j;
+
+                    for (j = 0; j < k; j++)
+                        if (x % num[j] != rem[j])
+                            break;
+
+                    if (j == k)
+                        return x;
+
+                    x++;
+                }
+            }
+        }
+    }
+}
